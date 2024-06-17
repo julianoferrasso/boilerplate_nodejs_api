0505b61925bf29bba815a987ae740e61dc155215
julianoferrasso <julianoferrasso@gmail.com>
2024-06-17 12:54:05 -0300

    REFACT: userController tratamento de erro

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,19 +3,25 @@ import prisma from '../prisma/client';
 import { AuthRequest } from '../middlewares/authMiddleware';
 
 export const getUserProfile = async (req: AuthRequest, res: Response) => {
-    const { email } = req.body
-    console.log('request = ' + req.token?.id + " - " + req.token?.email)
-
     try {
-        const user = await prisma.user.findUnique({
-            where: { email: email },
-        });
-
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
+        const { email } = req.body
+        if (!email) {
+            return res.status(400).json({ error: 'Email requerido' });
         }
-        res.status(200).json(user);
-    } catch (error) {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email: email },
+            });
+
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(400).json({ error: 'Something went wrong' });
+        }
+
+    } catch {
         res.status(400).json({ error: 'Something went wrong' });
     }
 };
