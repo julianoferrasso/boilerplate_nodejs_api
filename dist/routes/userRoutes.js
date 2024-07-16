0260e2443ce45e7d82cc52d0b5e25bf94566b456
Juliano <56641071+julianoferrasso@users.noreply.github.com>
2024-07-16 19:19:12 -0300

    Delete dist directory

--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.get('/profile', authMiddleware_1.authMiddleware, userController_1.getUserProfile);
-exports.default = router;
